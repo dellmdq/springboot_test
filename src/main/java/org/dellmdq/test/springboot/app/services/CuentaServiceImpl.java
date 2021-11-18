dfdbc4ed1d9f3a0ead3d11c23a4248964f4d2a19
dellmdq <erikdell@gmail.com>
2021-11-18 13:33:35 -0300

    CuentaController Added. Http Endpoints Added. TransaccionDTO Added. Transferir Controller Method Added.

--- src/main/java/org/dellmdq/test/springboot/app/services/CuentaServiceImpl.java
+++ src/main/java/org/dellmdq/test/springboot/app/services/CuentaServiceImpl.java
@@ -5,6 +5,7 @@ import org.dellmdq.test.springboot.app.models.Cuenta;
 import org.dellmdq.test.springboot.app.repositories.BancoRepository;
 import org.dellmdq.test.springboot.app.repositories.CuentaRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 
@@ -19,23 +20,27 @@ public class CuentaServiceImpl implements CuentaService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Cuenta findById(Long id) {
         return cuentaRepository.findById(id).orElseThrow();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public int revisarTotalTransferencias(Long bancoId) {
         Banco banco = bancoRepository.findById(bancoId).orElseThrow();
         return banco.getTotalTransferNumber();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public BigDecimal revisarSaldo(Long cuentaId) {
         Cuenta cuenta = cuentaRepository.findById(cuentaId).orElseThrow();
         return cuenta.getSaldo();
     }
 
     @Override
+    @Transactional
     public void transferir(Long numCuentaOrigen, Long numCuentaDestino, BigDecimal monto, Long bancoId) {
 
         Cuenta cuentaOrigen = cuentaRepository.findById(numCuentaOrigen).orElseThrow();
