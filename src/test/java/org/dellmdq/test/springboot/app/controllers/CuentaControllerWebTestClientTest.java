7cc2b1a4530c89a6bc1215f437d9138b8c3a043a
dellmdq <erikdell@gmail.com>
2021-11-29 20:40:20 -0300

    testTransferir agregado, se hace test de la respuesta JSON.

--- src/test/java/org/dellmdq/test/springboot/app/controllers/CuentaControllerWebTestClientTest.java
+++ src/test/java/org/dellmdq/test/springboot/app/controllers/CuentaControllerWebTestClientTest.java
@@ -1,6 +1,9 @@
 package org.dellmdq.test.springboot.app.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.dellmdq.test.springboot.app.models.TransaccionDTO;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,6 +12,8 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
@@ -20,10 +25,18 @@ class CuentaControllerWebTestClientTest {
     @Autowired
     private WebTestClient webTestClient;
 
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        objectMapper = new ObjectMapper();
+    }
+
     /**No es necesario usar un mapper para pasar el dto a json usando el object mapper
-     * el webTestClient se encarga de hacer ese trabajo.*/
+     * el webTestClient se encarga de hacer ese trabajo.
+     * Vamos a testear la respuesta JSON*/
     @Test
-    void testTransferir() {
+    void testTransferir() throws JsonProcessingException {
         //Given
         TransaccionDTO transaccionDTO = new TransaccionDTO();
         transaccionDTO.setCuentaOrigenId(1L);
@@ -31,8 +44,14 @@ class CuentaControllerWebTestClientTest {
         transaccionDTO.setBancoId(1L);
         transaccionDTO.setMonto(new BigDecimal("100"));
 
+        Map<String, Object> response = new HashMap<>();//armamos nuestro response
+        response.put("date", LocalDate.now().toString());
+        response.put("status", "OK");
+        response.put("mensaje", "Transferencia realizada con éxito.");
+        response.put("transaccion", transaccionDTO);
+
         //When
-        webTestClient.post().uri("http://localhost:8080/api/cuentas/transferir")
+        webTestClient.post().uri("http://localhost:8080/api/cuentas/transferencia")
                 .contentType(MediaType.APPLICATION_JSON)
                 .bodyValue(transaccionDTO)
                 //a cambio, a partir de aca viene la respuesta, lo que esperamos testear.
@@ -45,8 +64,11 @@ class CuentaControllerWebTestClientTest {
                 .jsonPath("$.mensaje").value( valor -> {
                     assertEquals("Transferencia realizada con éxito.", valor);
                 })
-                .jsonPath("$.mensaje").isEqualTo("Transferencia realizada con éxito")
-                .jsonPath("$transacción.cuentaOrigenId").isEqualTo(transaccionDTO.getCuentaOrigenId())
-                .jsonPath("$.date").isEqualTo(LocalDate.now().toString());
+                .jsonPath("$.mensaje").isEqualTo("Transferencia realizada con éxito.")
+                .jsonPath("$.transaccion.cuentaOrigenId").isEqualTo(transaccionDTO.getCuentaOrigenId())
+                .jsonPath("$.date").isEqualTo(LocalDate.now().toString())
+                //Ahora probamos con Map<String, Object> response. Necesitamos el object mapper.
+                //Convertimos map en un string con estructura json!
+                .json(objectMapper.writeValueAsString(response));
     }
 }
