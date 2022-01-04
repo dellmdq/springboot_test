01457493e0ee09de213851bd24f40426360ec565
dellmdq <erikdell@gmail.com>
2022-01-04 19:05:32 -0300

    testListar added

--- src/test/java/org/dellmdq/test/springboot/app/controllers/CuentaControllerWebTestClientTest.java
+++ src/test/java/org/dellmdq/test/springboot/app/controllers/CuentaControllerWebTestClientTest.java
@@ -16,9 +16,11 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static org.hamcrest.Matchers.*;
 
@@ -81,9 +83,7 @@ class CuentaControllerWebTestClientTest {
                 .jsonPath("$.mensaje").isNotEmpty()
                 .jsonPath("$.mensaje").value(is("Transferencia realizada con éxito."))
                 //lo mismo a la linea anterior pero con lambdas
-                .jsonPath("$.mensaje").value( valor -> {
-                    assertEquals("Transferencia realizada con éxito.", valor);
-                })
+                .jsonPath("$.mensaje").value( valor -> assertEquals("Transferencia realizada con éxito.", valor))
                 .jsonPath("$.mensaje").isEqualTo("Transferencia realizada con éxito.")
                 .jsonPath("$.transaccion.cuentaOrigenId").isEqualTo(transaccionDTO.getCuentaOrigenId())
                 .jsonPath("$.date").isEqualTo(LocalDate.now().toString())
@@ -120,8 +120,49 @@ class CuentaControllerWebTestClientTest {
                 .expectBody(Cuenta.class)
                 .consumeWith(response ->{
                    Cuenta cuenta = response.getResponseBody();
+                   assertNotNull(cuenta);
                    assertEquals("John", cuenta.getPersona());
                    assertEquals("2100.00",cuenta.getSaldo().toPlainString());
                 });
+    }
+
+    @Test
+    @Order(4)
+    void testListar() {
+        webTestClient.get().uri("/api/cuentas").exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$[0].persona").isEqualTo("Andrés")
+                .jsonPath("$[0].id").isEqualTo(1)
+                .jsonPath("$[0].saldo").isEqualTo(900)
+                .jsonPath("$[1].persona").isEqualTo("John")
+                .jsonPath("$[1].id").isEqualTo(2)
+                .jsonPath("$[1].saldo").isEqualTo(2100)
+                .jsonPath("$").isArray()
+                .jsonPath("$").value(hasSize(2));
+    }
+
+    @Test
+    @Order(5)
+    void testListar2() {
+        webTestClient.get().uri("/api/cuentas").exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBodyList(Cuenta.class)
+                .consumeWith(response -> {
+                    List<Cuenta> cuentas = response.getResponseBody();
+                    assertNotNull(cuentas);
+                    assertEquals(2, cuentas.size());
+                    assertEquals(1L, cuentas.get(0).getId());
+                    assertEquals("Andrés", cuentas.get(0).getPersona());
+                    assertEquals(900, cuentas.get(0).getSaldo().intValue());//expected como entero
+                    assertEquals(2L, cuentas.get(1).getId());
+                    assertEquals("John", cuentas.get(1).getPersona());
+                    assertEquals("2100.0", cuentas.get(1).getSaldo().toPlainString());//expected como string
+                })
+                //distintas formas y modulos para testear tamaño de la lista
+                .hasSize(2)
+                .value(hasSize(2));
     }
 }
